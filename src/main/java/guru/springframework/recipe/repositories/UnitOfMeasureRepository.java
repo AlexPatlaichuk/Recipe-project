ce21205a5dc77159f7215fb79323e9926ff4ff0a
Alex Patlaichuk <129276189+AlexPatlaichuk@users.noreply.github.com>
2023-04-24 12:03:11 +0300

    added repository interfaces

---
+++ src/main/java/guru/springframework/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.recipe.repositories;
+
+import guru.springframework.recipe.controllers.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
